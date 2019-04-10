5248c109720a72f70bfe1e8fb93cf1242f8e1dc1
4hmetuyar <4hmetuyar@gmail.com>
2019-04-10 14:51:38 +0300

    Account Login.

---
+++ QuestionCreation.Web.AdminUI/QuestionCreation.Web.AdminUI/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace QuestionCreation.Web.AdminUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginModel loginModel)
+        {
+            return View();
+        }
+    }
+}
---
+++ QuestionCreation.Web.AdminUI/QuestionCreation.Web.AdminUI/Models/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace QuestionCreation.Web.AdminUI.Controllers
+{
+    public class LoginModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
