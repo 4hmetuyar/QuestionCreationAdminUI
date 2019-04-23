018f175d6531ebdb04e57bb656b72073b2150ec7
4hmetuyar <4hmetuyar@gmail.com>
2019-04-23 16:33:46 +0300

    Base Controller fixed.

--- QuestionCreation.Web.AdminUI/QuestionCreation.Web.AdminUI/Controllers/BaseController.cs
+++ QuestionCreation.Web.AdminUI/QuestionCreation.Web.AdminUI/Controllers/BaseController.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace QuestionCreation.Web.AdminUI.Controllers
 {
     public class BaseController : Controller
     {
-        // GET: Base
-        public ActionResult Index()
-        {
-            return View();
-        }
+      
     }
 }
--- QuestionCreation.Web.AdminUI/QuestionCreation.Web.AdminUI/Controllers/HomeController.cs
+++ QuestionCreation.Web.AdminUI/QuestionCreation.Web.AdminUI/Controllers/HomeController.cs
@@ -2,7 +2,7 @@
 
 namespace QuestionCreation.Web.AdminUI.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         public ActionResult Index()
         {
