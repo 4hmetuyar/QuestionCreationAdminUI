e1041e2131b7c9e343c5531d55ef95c457f52098
4hmetuyar <4hmetuyar@gmail.com>
2019-04-25 17:12:41 +0300

    Question Controller Added.

---
+++ QuestionCreation.Web.AdminUI/QuestionCreation.Web.AdminUI/Controllers/QuestionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuestionCreation.Web.AdminUI.Controllers
+{
+    public class QuestionController : BaseController
+    {
+        // GET: Question
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
